7111c717d0e5df78dfa4151ce115f79f5dcfa3c5
KennedyNvsf <kennedynvsf@gmail.com>
2021-07-05 14:47:24 +0100

    base setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles/app.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Lets build Gmail</h1>
+    </div>
+  );
+}
+
+export default App;
